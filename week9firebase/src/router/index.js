86b38d66608e238a99a6444be8f2bd20b48d3b24
akar0096 <akar0096@student.monash.edu>
2025-10-24 22:46:58 +1100

    deployment 4

--- week9firebase/src/router/index.js
+++ week9firebase/src/router/index.js
@@ -1,18 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import FirebaseSigninView from "@/views/FirebaseSigninView.vue"
-import FirebaseRegisterView from "@/views/FirebaseRegisterView.vue"
-import AdminLogin from '@/views/AdminLogin.vue'
-import AdminReg from '@/views/AdminReg.vue'
-import GetBookCountView from '@/views/GetBookCountView.vue'
-import AddBookView from '@/views/AddBookView.vue'
-import WeatherView from '@/views/WeatherView.vue'
-import CountBookAPI from '@/views/CountBookAPI.vue'
-import GetAllBookAPI from '@/views/GetAllBookAPI.vue'
+// ... other imports
 
 const routes = [
   { path: '/', redirect: '/firelogin' },
-  { path: '/addbook', name: 'AddBook', component: AddBookView },
   { path: '/firelogin', name: 'FireLogin', component: FirebaseSigninView },
+  { path: '/addbook', name: 'AddBook', component: AddBookView },
   { path: '/getallbookapi', name: 'GetAllBookAPI', component: GetAllBookAPI },
   { path: '/countbookapi', name: 'CountBookAPI', component: CountBookAPI },
   { path: '/getbookcountview', name: 'GetBookCountView', component: GetBookCountView },
@@ -23,8 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  // Hash mode avoids 404 issues on GitHub Pages
-  history: createWebHashHistory('/35215704-efolio11-12/'),
+  history: createWebHashHistory('/35215704-efolio11-12/week9firebase/'),
   routes
 })
 
