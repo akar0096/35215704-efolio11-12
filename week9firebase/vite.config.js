d3a2a9b8727567fb5bb2bf1a66d6a64a31f4e936
akar0096 <akar0096@student.monash.edu>
2025-10-24 21:53:19 +1100

    Trigger deployment1

--- week9firebase/vite.config.js
+++ week9firebase/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/week9firebase/',
   plugins: [
     vue(),
     vueDevTools(),
